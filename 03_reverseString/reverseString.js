65e8f0bc46d674bf1e69e16cf5a61d8c127d3372
rutabageldev <187210498+rutabageldev@users.noreply.github.com>
2024-11-10 21:18:13 -0500

    Completed exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,10 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    let inputChar = input.split("");
+    let output ="";
+    for (let i = inputChar.length - 1; i >= 0; i--) {
+        output = output + inputChar[i];
+    }
+    return output;
 
 };
 
