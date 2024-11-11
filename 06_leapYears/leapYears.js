795c2d8a57db8277c9b470d7198344544893a388
rutabageldev <187210498+rutabageldev@users.noreply.github.com>
2024-11-11 11:16:28 -0500

    Completed exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,20 @@
-const leapYears = function() {
+/*function leapYears(year)
+    if (year is divisible by 400)
+        return true
+    else if (year is divisible by 4 AND year is not divisible by 100)
+        return true
+    else
+        return false
+     */
+
+const leapYears = function(year) {
+    if (year % 400 == 0) {
+        return true;
+    } else if(year % 4 == 0 && year % 100 != 0) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
