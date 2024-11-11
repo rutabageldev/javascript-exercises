cfaf64803576f8daee3b5e1c4dfeb8f3eaa1b68a
rutabageldev <187210498+rutabageldev@users.noreply.github.com>
2024-11-10 21:11:20 -0500

    Completed exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
+const repeatString = function(input, qty) {
+    let output ='';
+    if (qty < 0) {
+        return 'ERROR';
+    } else {
+        for (let i = 0; i <qty; i++) {
+            output = output + input;
+        }
+    } 
+    return output;
+    };
 
 // Do not edit below this line
 module.exports = repeatString;
