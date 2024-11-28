89c93d1f4ec0f28c66b9cdc739fb6fb006cf633c
rutabageldev <187210498+rutabageldev@users.noreply.github.com>
2024-11-28 16:12:59 -0500

    Basic implementation of fibonnaci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,34 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    let validInput = testInput(input);
+    if (validInput){
+        return createFibonacci(input);
+    } else {
+        return "Oops!";
+    }
 };
+
+function testInput(temp) {
+    return parseInt(temp) >= 0;
+}
+
+function createFibonacci (target) {
+    const endValue = parseInt(target);
+    let fibValue = 1;
+    let prevFibValue = 1;
+    if (endValue == 0) {
+        return 0;
+    } else {
+        if (endValue == 1 || endValue == 2){
+            return 1;
+        }
+        for (let i = 3; i <= endValue; i++) {
+            let temp = prevFibValue;
+            prevFibValue = fibValue;
+            fibValue += temp;
+        }
+    }
+    return fibValue
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
