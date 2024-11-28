22aa8538c1a9b78e2252e517e9bc0327e03f3515
rutabageldev <187210498+rutabageldev@users.noreply.github.com>
2024-11-28 14:16:52 -0500

    Completed Calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -8,14 +8,18 @@ const subtract = function(a, b) {
 
 const sum = function(arr) {
 	let sum = 0;
-  for (let i = 0; i++; i <= arr.length-1){
-    sum += arr[i];
+  for (let i = 0; i < arr.length; i++){
+    sum += parseInt(arr[i]);
   }
   return sum;
 };
 
-const multiply = function(a, b) {
-  return a * b;
+const multiply = function(arr) {
+  let result = 1;
+  for (let i = 0; i < arr.length; i++){
+    result *= parseInt(arr[i]);
+  }
+  return result;
 };
 
 const power = function(a, b) {
@@ -24,9 +28,10 @@ const power = function(a, b) {
 
 const factorial = function(a) {
 	let result = 1;
-  for (let i = 0; i++; i = a){
-    result = result * i;
+  for (let i = a; i > 0; i--){
+    result = result * parseInt(i);
   }
+  return result;
 };
 
 
