9b96676322eb85df210f4ac6a5aaffc3efbd6f63
rutabageldev <187210498+rutabageldev@users.noreply.github.com>
2024-11-28 15:20:28 -0500

    Simplified Calculator solutions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -7,20 +7,17 @@ const subtract = function(a, b) {
 };
 
 const sum = function(arr) {
-	let sum = 0;
-  for (let i = 0; i < arr.length; i++){
-    sum += parseInt(arr[i]);
-  }
+  const sum = arr.reduce((total, i) => {
+    return total + i;
+  }, 0)
   return sum;
 };
 
 const multiply = function(arr) {
-  let result = 1;
-  for (let i = 0; i < arr.length; i++){
-    result *= parseInt(arr[i]);
-  }
-  return result;
-};
+  return arr.reduce((accum, i) => {
+    return accum * i;
+  }, 1);
+}
 
 const power = function(a, b) {
 	return a ** b;
