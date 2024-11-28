856f3aae13fdaed34f78fdfa4d6b2140aa9c6aa1
rutabageldev <187210498+rutabageldev@users.noreply.github.com>
2024-11-28 14:56:37 -0500

    Completed Palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    input = cleanString(input);
+    let reverseString =input.split("").reverse().join("");
+    return input === reverseString
 };
+
+function cleanString(str){
+    const allowed = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let temp = str.toLowerCase();
+    let tempArray = temp.split("");
+    let cleanArray = tempArray.filter(char => allowed.includes(char));
+    return cleanArray.join("");
+}
 
 // Do not edit below this line
 module.exports = palindromes;
