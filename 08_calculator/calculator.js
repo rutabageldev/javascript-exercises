a101e58606976aa8b4c7fe0b29b08c225d1280ae
rutabageldev <187210498+rutabageldev@users.noreply.github.com>
2024-11-28 13:50:45 -0500

    Implemented add and subtract to Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;	
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  for (let i = 0; i++; i <= arr.length-1){
+    sum += arr[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(a, b) {
+  return a * b;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let result = 1;
+  for (let i = 0; i++; i = a){
+    result = result * i;
+  }
 };
+
+
 
 // Do not edit below this line
 module.exports = {
