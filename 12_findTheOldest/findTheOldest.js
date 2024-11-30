7114d790cda8e0e529f4e6bca492f8bcc4c26667
rutabageldev <187210498+rutabageldev@users.noreply.github.com>
2024-11-29 20:05:03 -0500

    Completed exerciese 12, findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldestPerson, person) => {
+        const currentYear = new Date().getFullYear();
+        const currentAge = (person.yearOfDeath || currentYear)-person.yearOfBirth;
+        const oldestAge = (oldestPerson.yearOfDeath || currentYear) - oldestPerson.yearOfBirth;
+        
+        return currentAge > oldestAge ? person : oldestPerson;
+    });
 };
 
+function calculateAge(person) {
+    return person.yearOfDeath - person.yearOfBirth;
+}
 // Do not edit below this line
 module.exports = findTheOldest;
